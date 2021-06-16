6f872b194a337c7e273f027cee6131f6808f6474
Sara <sara.m.adamski@gmail.com>
2021-06-16 18:31:40 -0500

    put routes for category and tag

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -52,6 +52,27 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+  .then(updatedCategoryData => {
+    if (!updatedCategoryData) {
+      res.status(404).json({ message: 'No category found with this id' });
+      return;
+    }
+    res.json(updatedCategoryData)
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
