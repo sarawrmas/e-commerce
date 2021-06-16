f8b18f7fb5ff530be2a0e9ca73508c72ccd9041d
Sara <sara.m.adamski@gmail.com>
2021-06-16 11:53:21 -0500

    models and associations

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,29 @@
 require('dotenv').config();
-
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+// when app is deployed, it will have access to Heroku's JawsDB URL variable and use that value to connect
+if(process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  // when not deployed, use localhost configuration
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
       },
-    });
+      port: 3306
+  });
+}
+
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//       host: 'localhost',
+//       dialect: 'mysql',
+      // dialectOptions: {
+      //   decimalNumbers: true,
+      // },
+//     });
 
 module.exports = sequelize;
